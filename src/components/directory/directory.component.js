b5713e750dd499bcaf8a9f7f5d49e54a79aa005e
Lane876 <milenkovicmilan10@gmail.com>
2020-03-12 22:41:18 +0100

    update2

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -12,33 +12,38 @@ class Directory extends Component {
           title: "hats",
           imageUrl:
             "https://images.pexels.com/photos/35185/hats-fedora-hat-manufacture-stack.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-          id: 1
+          id: 1,
+          linkUrl: "hats"
         },
         {
           title: "JACKETS",
           imageUrl:
             "https://images.pexels.com/photos/16170/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=9400",
-          id: 2
+          id: 2,
+          linkUrl: ""
         },
         {
           title: "SNEAKERS",
           imageUrl:
             "https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-          id: 3
+          id: 3,
+          linkUrl: ""
         },
         {
           title: "WOMENS",
           imageUrl:
             "https://images.pexels.com/photos/3812413/pexels-photo-3812413.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           id: 4,
-          size: "large"
+          size: "large",
+          linkUrl: ""
         },
         {
           title: "mens",
           imageUrl:
             "https://images.pexels.com/photos/3846456/pexels-photo-3846456.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           id: 5,
-          size: "large"
+          size: "large",
+          linkUrl: ""
         }
       ]
     };
@@ -47,8 +52,8 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
