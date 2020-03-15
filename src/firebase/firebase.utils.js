afe62afa302e01253ee14de17c5f6e4a0dc7fe73
Lane876 <milenkovicmilan10@gmail.com>
2020-03-15 23:35:49 +0100

    auth update

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyAWFQfFuCXAEDJKTCZS2kgAt2c1XjCzv4o",
+  authDomain: "crown-db-aa592.firebaseapp.com",
+  databaseURL: "https://crown-db-aa592.firebaseio.com",
+  projectId: "crown-db-aa592",
+  storageBucket: "crown-db-aa592.appspot.com",
+  messagingSenderId: "840610710143",
+  appId: "1:840610710143:web:74989e41e488173494f54a",
+  measurementId: "G-RRMWJ42KY8"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
