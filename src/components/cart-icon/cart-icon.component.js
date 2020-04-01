93f5c45e57c2ccdefa1e3ec9cd931aff571cbc3d
Lane876 <milenkovicmilan10@gmail.com>
2020-04-01 20:33:33 +0200

    reselect

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -4,6 +4,7 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import "./cart-icon.styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+import { createStructuredSelector } from "reselect";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
@@ -19,8 +20,8 @@ const mapDispatchToProps = dispatch => ({
 });
 
 //brojac na ikonici za stvari koje su u korpi
-const mapStateToProps = state => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
