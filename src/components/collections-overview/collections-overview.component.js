b8e72d0735dbd3718539e2c0abdcb023ef7fdada
Lane876 <milenkovicmilan10@gmail.com>
2020-04-06 19:44:05 +0200

    data normalization

--- src/components/collections-overview/collections-overview.component.js
+++ src/components/collections-overview/collections-overview.component.js
@@ -3,9 +3,9 @@ import "./collections-overview.style.scss";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../collection-preview/collection-preview.component";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
-const CollectonsOverview = ({ collections }) => {
+const CollectionsOverview = ({ collections }) => {
   return (
     <div className="collections-overview">
       {collections.map(({ id, ...otherCollectionProps }) => (
@@ -16,7 +16,7 @@ const CollectonsOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
-export default connect(mapStateToProps)(CollectonsOverview);
+export default connect(mapStateToProps)(CollectionsOverview);
