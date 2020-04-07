5108ac5c6122d211b20d38b0c1592d85dc9e3cd2
Lane876 <milenkovicmilan10@gmail.com>
2020-04-07 16:24:06 +0200

    stripe integration

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -4,9 +4,10 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import {
   selectCartItems,
-  selectCartTotal
+  selectCartTotal,
 } from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({ cartItems, total }) => {
   return (
@@ -28,19 +29,25 @@ const CheckoutPage = ({ cartItems, total }) => {
           <span>Remove</span>
         </div>
       </div>
-      {cartItems.map(cartItem => (
+      {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use this credit card data for testing
+        <br />
+        4242 4242 4242 4242 - Exp: 12/20 CVV:123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
-  total: selectCartTotal
+  total: selectCartTotal,
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
